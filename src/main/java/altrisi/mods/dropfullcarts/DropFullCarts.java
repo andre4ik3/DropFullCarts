a0db6177460680854d93d6953dc349965319d7fa
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-07-04 22:45:10 +0300

    forge

---
+++ src/main/java/altrisi/mods/dropfullcarts/DropFullCarts.java
@@ -0,0 +1,10 @@
+package altrisi.mods.dropfullcarts;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("dropfullcarts")
+public class DropFullCarts {
+    public DropFullCarts() {
+        /* do nothing */
+    }
+}
